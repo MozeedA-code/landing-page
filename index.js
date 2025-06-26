e3ecfe77e93c4d89ed530cb78e73b41d16954908
Mozeed Arogundade <mozeedarogundade02@gmail.com>
2025-06-26 09:56:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function contactClick() {
+  const contactSection = document.querySelector(".contact-box");
+  contactSection.scrollIntoView({ behavior: "smooth" });
+}
+
+function aboutID() {
+  const aboutSection = document.getHTML("about.html");
+  aboutSection.getHTML("about.html");
+}
